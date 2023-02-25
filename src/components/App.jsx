a37acb7b2819f504b2dee490b0f7fafd72162ab8
diana.maliarchuk <diana.maliarchuk.work@gmail.com>
2023-02-25 22:27:12 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,13 @@ import { GlobalStyle } from 'GlobalStyle';
 import { Layout } from './Layout/Layout.styled';
 import { Component } from 'react';
 import { ContactsForm } from './Form/Form';
-
-
+import { Filter } from './Filter/Filter';
+import { ContactsList } from './ContactsList/ContactsList';
 
 export class App extends Component{
 
   state = {
-    contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
 
@@ -26,23 +21,36 @@ export class App extends Component{
   };
 
   handleChange = evt => {
-    this.setState({ inputValue: evt.target.value });
+    this.setState({ filter: evt.target.value });
+  };
+  
+  filterContact = (contactsArr) => {
+    return contactsArr.filter(contact => contact.name.toLowerCase().includes(this.state.filter.toLowerCase()));
   }
 
+  checkName = name => {
+    if(this.state.contacts.find(contact => contact.name === name)){
+      alert(`${name} is already in contacts!`)
+    }
+  }
+
+  deleteContacts = name => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.name !== name),
+      };
+    });
+  }
+  
   render(){
-    const { filterValue } = this.state;
+    const { filter: filterValue } = this.state;
      return (
       <Layout>
         <h1>Phonebook</h1>
-        <ContactsForm onSave = {this.addContact}></ContactsForm>
-
+        <ContactsForm onSave={this.addContact} contacts={this.state.contacts}></ContactsForm>
         <h2>Contacts</h2>
-
-        <input onChange={this.handleChange}  value={filterValue} type="text" name="filter"/> 
-
-        <ul> 
-          {this.state.contacts.map(contact => <li key={contact.id}>  {contact.name}: {contact.number}</li>)}
-        </ul>
+        <Filter onChange={this.handleChange} value={filterValue}/> 
+        <ContactsList contactsList={this.filterContact(this.state.contacts)} onClickDelete={this.deleteContacts}/>
         <GlobalStyle/>
       </Layout>
     );
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,37 +1,53 @@
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import { Formik, Field } from 'formik';
+import * as yup from 'yup';  
+import 'yup-phone';
 import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import {Form, ErrorFormik, Button} from './Form.styled';
 
-const Schema = Yup.object().shape({
-    name: Yup.string("Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan").required('Required'),
-    number: Yup.number().required(),
+const Schema = yup.object().shape({
+    name: yup.string("Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan").required(),
+    number: yup.number().required(),
   });
 
-export const ContactsForm = ({onSave}) => {
+export const ContactsForm = ({onSave, contacts}) => {
     return(
         <Formik 
         initialValues={{ contacts: [], name: '', number: '' }}
         validationSchema = {Schema}
         onSubmit={(values, actions) => {
-            // const = 
-            onSave({
+            if(contacts.find(contact => contact.name === values.name)){
+              alert(`${values.name} is already in contacts!`)
+            }else{
+              onSave({
                 id: nanoid(),
                 name: values.name, 
                 number: values.number,
-            });
+              });
+            }
+            
             actions.resetForm();
         }}>
           <Form>
             <label htmlFor="name">Name</label>
             <Field name="name" type="text" />
-            <ErrorMessage name="name" />
+            <ErrorFormik name="name" component="span"/>
 
             <label htmlFor="number">Number</label>
             <Field name="number" type="tel" />
-            <ErrorMessage name="number" />
+            <ErrorFormik name="number" component="span" />
 
-            <button type="submit" >Add contact</button>
+            <Button type="submit" >Add contact</Button>
           </Form>
         </Formik>
     )
+}
+
+ContactsForm.propTypes = {
+  onSave: PropTypes.func.isRequired,
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+        name: PropTypes.string.isRequired,
+    }).isRequired
+).isRequired,
 }
